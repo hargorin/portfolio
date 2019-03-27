b916c8613ac4ee9e209a8f4ca243f7b38b793ff4
¨Jan <janstocker@gmx.ch>
2019-03-27 11:21:08 +0100

    edit CNAME directory

--- publish_ghpages.sh
+++ publish_ghpages.sh
@@ -22,7 +22,7 @@ echo "Generating site"
 hugo
 
 echo "Generating CNAME"
-echo janstocker.ch >> CNAME
+echo janstocker.ch >> public/CNAME
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages"
